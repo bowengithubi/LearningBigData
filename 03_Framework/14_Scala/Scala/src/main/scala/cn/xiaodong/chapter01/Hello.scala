8b2e30a6de0976eaa1cb5c4e84d649ead3c924d3
zhuxiaodongwx <zhuxiaodongwx@126.com>
2021-09-12 21:43:37 +0800

    Scala学习，项目代码初始化

---
+++ 03_Framework/14_Scala/Scala/src/main/scala/cn/xiaodong/chapter01/Hello.scala
@@ -0,0 +1,18 @@
+package cn.xiaodong.chapter01
+
+/**
+ * Scala入门案例
+ */
+object Hello {
+  /**
+   * 主方法
+   * @param args
+   */
+  def main(args: Array[String]): Unit = {
+    // scala代码
+    println("Hello Scala");
+
+    // java代码
+    System.out.println("Hello Scala");
+  }
+}
